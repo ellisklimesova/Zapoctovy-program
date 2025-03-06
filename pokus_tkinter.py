9c69595f5be7970d2d1fda1b9c82f835190965d6
eli <klimesova.ell@gmail.com>
2025-03-06 15:45:54 +0100

    add increasing buttons

--- pokus_tkinter.py
+++ pokus_tkinter.py
@@ -1,11 +1,7 @@
 """
 Keywords
--- zavrit do class all of the below
--- canvas
+
 -- slider (potahovatko)
--- policko s tlacitky doleva doprava
-    - kdyz cislo ok, kdyz necislo tak ignoruj
-    - doprava zvetsi o 1, doleva zmensi o 1, spravne cislo zobraz v policku
 -- timer - spousti eventy 
 """
 """
@@ -20,6 +16,7 @@ class Program:
     def __init__(self):
         self.root = Tk()
         self.set_window()
+        self.frame_number = 0
         self.root.mainloop()
 
     def set_window(self):
@@ -36,17 +33,19 @@ class Program:
 
 
         # creating a label for name using widget Label
-        name_label = Label(self.root, text = 'Username', font=('calibre',10, 'bold'))
+        name_label = Label(self.root, text = 'Frame number', font=('calibre',10, 'bold'))
         name_label.pack()
 
         # creating a entry for input name using widget Entry
-        self.frame_number = IntVar()
-        name_entry = Entry(self.root, textvariable = self.frame_number, font=('calibre',10,'normal'))
-        name_entry.pack()
+        # self.frame_number = IntVar()
+        self.num_entry = Entry(self.root, font=('calibre',10,'normal'))
+        self.num_entry.pack()
+        self.num_entry.bind("<KeyRelease>", self.submit_frame_number)
 
-        # creating a button using the widget Button that will call the submit function 
-        sub_btn = Button(self.root, text = 'Submit', command = self.submit_frame_number) 
-        sub_btn.pack()
+        subframe = Frame().pack()
+        # button that increases the number
+        Button(subframe, text = '+1', command = self.increase_frame_number).pack(side=LEFT)
+        Button(subframe, text = '-1', command = lambda: self.increase_frame_number(False)).pack(side=LEFT)
 
         # creating canvas
         c = Canvas(self.root, bg="gray", height=500, width=500)
@@ -60,12 +59,34 @@ class Program:
         # s = Scale(root, from_=0, to=200, orient=HORIZONTAL)
         # s.pack()
 
-        # function that will get the name and print it on the screen
-    def submit_frame_number(self): # on this level it has to have self
 
-        number = self.frame_number.get()
-        print(f"Frame is : {number}")
-        self.frame_number.set(number)
+    def submit_frame_number(self, event=None): # on this level it has to have self
+
+        number_str = self.num_entry.get()
+        if number_str.isnumeric():                           
+            number = int(number_str)       # No need to save (no self. ), as not used elsewhere                              
+        else:     
+            number = self.frame_number
+            
+        print(f"Number of frame: {number}")
+        self.num_entry.delete(0,len(self.num_entry.get()))
+        self.num_entry.insert(0, number)
+        self.frame_number = number 
+
+    def increase_frame_number(self, sign=True):
+        number_str = self.num_entry.get()
+        if number_str.isnumeric():                           
+            number = int(number_str)            
+        else:     
+            number = self.frame_number
+        number += 1 if sign else -1  
+            
+        self.num_entry.delete(0,len(self.num_entry.get()))
+        self.num_entry.insert(0, number)
+        self.frame_number = number 
+        print(f"Increased by 1: {number}")
+
+        # self.display_frame()  # will show the new canvas
 
 if __name__ == "__main__":
     p = Program()
